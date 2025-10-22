9f2af7f7a5b3ccff757faec8ccaf67bf5d7828ef
amit-nz <amit@focis.nz>
2025-10-22 13:02:38 +1300

    Update o365-exchange-find-SMTP-address.ps1
    add cmdlet to enable wildcard search

--- o365-exchange-find-SMTP-address.ps1
+++ o365-exchange-find-SMTP-address.ps1
@@ -5,3 +5,9 @@ Connect-ExchangeOnline -ShowProgress $true
 
 # Search for the SMTP address
 Get-Recipient -Filter "EmailAddresses -like 'smtp:hello@domain.com'"
+
+# Or, print a list of everyone that has an emai laddres "like":
+Get-Recipient -ResultSize Unlimited -Filter "EmailAddresses -like '*domain.com'" | Select-Object DisplayName, @{Name='AllSMTPAddresses';
+                  Expression={($_.EmailAddresses |
+                                Where-Object {$_ -match '^smtp:'} |
+                                ForEach-Object {$_ -replace '^smtp:',''}) -join ', '}}
